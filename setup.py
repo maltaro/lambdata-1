3f28e700727c7824cf51e41dbefe81ee306c4468
jwross24 <jwross24@users.noreply.github.com>
2020-01-13 13:47:23 -0500

    update github URL

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://lambdaschool.com/courses/data-science",
+    url="https://github.com/jwross24",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires = REQUIRED,
